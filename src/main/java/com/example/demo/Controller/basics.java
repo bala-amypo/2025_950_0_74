114cc7cbd418ea4a905d1deb8f771c3f616ca39b
bala-amypo <bala@amypo.in>
2025-12-17 05:25:12 +0000

    User commit

--- src/main/java/com/example/demo/Controller/basics.java
+++ src/main/java/com/example/demo/Controller/basics.java
@@ -1,9 +1,29 @@
-package com.example.demo.controller;
-import org.springframework.web.bind.annotation.*;
-@RestController
-public class basics{
-    @GetMapping("/home")
-    public String name(){
-        return"hello world";
+import jakarta.persistence.*;
+
+public class student{
+    @ID
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private INTEGER id;
+    private String name;
+    private String email;
+
+    public int getId(){
+        return id;
     }
+    public String getName(){
+        return name;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public void setId(int id){
+        this.id=id;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+    public void setEmail(String email){
+        this.email=email;
+    }
+
 }
